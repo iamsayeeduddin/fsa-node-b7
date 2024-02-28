c6b318f964670cb948dec55ccf73c7fb159895c0
Sayeeduddin <iamsayeeduddin@outlook.com>
2024-02-28 20:18:14 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const userData = require("./data.json");
+
+const data = ["Book1", "Book2", "Book3"];
+
+const PORT = process.env.PORT || 5000;
+
+function ctrl(req, res) {
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      res.write("Welcome to My API!");
+      break;
+
+    case "/books":
+      res.writeHead(JSON.stringify(data));
+      res.write("Welcome to My API!");
+      break;
+
+    default:
+      res.writeHead(404);
+      break;
+  }
+  res.end();
+}
+
+http.createServer(ctrl).listen(PORT);
